19662a51dbd53a2839625914f037eba4039fc9f5
tartley <tartley@gmail.com>
2009-08-08 10:20:36 +0000

    v0.1.9 include README.rst in MANIFEST.in, because otherwise setup.py chokes trying to read it (eg. on install)

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ from os.path import dirname, join
 
 from svgbatch import __version__
 
-readme = join(dirname(__file__), 'docs', 'README.rst')
-long_description = open(readme).read()
+def get_long_description():
+    readme = join(dirname(__file__), 'docs', 'README.rst')
+    return open(readme).read()
 
 setup(
     name='svgbatch',
     version=__version__,
     description=
       'Loads SVG files into pyglet Batch objects for OpenGL rendering.',
-    long_description=long_description,
+    long_description=get_long_description(),
     url='http://code.google.com/p/svgload/',
     license='BSD',
     author='Jonathan Hartley',
